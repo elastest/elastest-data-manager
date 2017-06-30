df46492d53c37d3535d865a4c465eb85cef241ce
Savas Gioldasis <savas.gioldasis@upstreamsystems.com>
2017-06-30 18:30:04 +0300

    CHG: mysql/data

--- bin/teardown-linux.sh
+++ bin/teardown-linux.sh
@@ -6,7 +6,7 @@ docker-compose  --project-name edm down
 rm -rf alluxio/data
 mkdir alluxio/data
 
-rm -rf mysql/data/*
+sudo rm -rf mysql/data/*
 
 rm -rf elasticsearch/esdata1/nodes
 rm -rf elasticsearch/esdata2/nodes
