c518f3ca87165ce82d9ce7e31b8016329c4e4e71
Savas Gioldasis <s.gioldasis@gmail.com>
2017-07-01 20:38:30 +0300

    ADD: Windows scripts

---
+++ bin/startup-windows.ps1
@@ -0,0 +1,6 @@
+﻿Write-Host  "Starting up using docker-compose"
+
+docker network rm elastest
+docker network create -d bridge elastest
+
+docker-compose --project-name edm   up -d
---
+++ bin/teardown-windows.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host   "Tearing down using docker-compose"
+
+docker-compose  --project-name edm down
+
+rm -r alluxio\data\*
+rm -r mysql\data\*
+rm -r elasticsearch\esdata1\*
+rm -r elasticsearch\esdata2\*
+
+docker volume prune -f
