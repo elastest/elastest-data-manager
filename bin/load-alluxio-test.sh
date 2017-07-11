da3c9038725c2438c02aabb5f17b794b3665fb6a
Savas Gioldasis <savas.gioldasis@upstreamsystems.com>
2017-07-11 17:19:30 +0300

    CHG: Load Scripts

--- bin/load-alluxio-test.sh
+++ bin/load-alluxio-test.sh
@@ -17,4 +17,6 @@ curl -v -X POST http://localhost:39999/api/v1/paths//hdfs/test.txt/create-file
 curl -v -X POST http://localhost:39999/api/v1/streams/2/write --data-binary @test.txt -H "Content-Type: application/octet-stream"
 curl -v -X POST http://localhost:39999/api/v1/streams/2/close
 
+rm test.txt
+
 echo "Test data has loaded ..."
