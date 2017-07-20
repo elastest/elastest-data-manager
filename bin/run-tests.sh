1f725bf2336e5ff3922461182aa9e552e367de78
Savas Gioldasis <s.gioldasis@gmail.com>
2017-07-20 11:56:15 +0300

    CHG: Add HTML coverage report

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -5,3 +5,5 @@ echo "Running all tests !!!"
 #docker-compose -p edm exec rest-api nose2 --with-coverage --verbose
 # docker-compose -p edm exec rest-api nose2 --verbose
 docker-compose -p edm exec rest-api nose2 --with-coverage --coverage-report term-missing
+
+#docker-compose -p edm exec rest-api nose2 --with-coverage --coverage-report=html
