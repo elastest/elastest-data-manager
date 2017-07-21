590e852e1e9e879cbcd1d98e816c703c9bae9f67
Savas Gioldasis <savas.gioldasis@upstreamsystems.com>
2017-07-21 19:29:01 +0300

    ADD: Cobertura

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -8,3 +8,6 @@ echo "Running tests !!!"
 #docker-compose -p edm exec rest-api nose2 --with-coverage --coverage-report term-missing
 
 docker-compose -p edm exec rest-api tox
+# docker-compose -p edm exec rest-api codecov -t a07586d2-4aed-4a0d-819c-35520f5eda54
+
+#docker-compose -p edm exec codecov -t a07586d2-4aed-4a0d-819c-35520f5eda54
