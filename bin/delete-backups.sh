64b545b7215e38af4c4d53428d4b30b1e06a8ce3
Savas Gioldasis <s.gioldasis@gmail.com>
2017-07-24 16:48:54 +0300

    CHG: Use official MySQL docker image

--- bin/delete-backups.sh
+++ bin/delete-backups.sh
@@ -13,9 +13,13 @@ touch backup/hdfs/.gitkeep
 
 sudo rm rest/rest_api_project/rest_api_app/db.sqlite
 sudo rm rest/rest_api_project/tests/db.sqlite
+
 sudo rm rest/rest_api_project/.coverage
+sudo rm rest/rest_api_project/coverage.xml
 
 sudo rm -rf rest/rest_api_project/.tox
 sudo rm -rf rest/rest_api_project/edm.egg-info
 sudo rm rest/rest_api_project/nosetests.xml
+
+rm logs.jsonl
 
