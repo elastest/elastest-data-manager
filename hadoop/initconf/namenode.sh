30363b6e83c8be57014e135fc809d1c05442bb24
Nikos Gavalas <ns.gavalas@gmail.com>
2018-02-01 12:46:12 +0200

    final commit for nfs. TODO: add a webdav (or similar) server to namenode

--- hadoop/initconf/namenode.sh
+++ hadoop/initconf/namenode.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# This is a wrapper script, to start the namenode together with the NFS sharing service. 
+# This is a wrapper script, to start the namenode together with the NFS sharing  
+service. 
 # Author: Nick Gavalas
 
 
-/opt/hadoop/sbin/hadoop-daemon.sh --script hdfs namenode
-/opt/hadoop/sbin/hadoop-daemon.sh --script hdfs start portmap
-/opt/hadoop/sbin/hadoop-daemon.sh --script hdfs start nfs3
+/usr/local/hadoop/sbin/hadoop-daemon.sh --script hdfs start namenode
+/usr/local/hadoop/sbin/hadoop-daemon.sh --script hdfs start portmap
+/usr/local/hadoop/sbin/hadoop-daemon.sh --script hdfs start nfs3
+
+# sleep for some time, to allow services to start. Unfortunately this is 
+sleep 20
 mount -t nfs -o vers=3,proto=tcp,nolock,noacl,sync 127.0.0.1:/ /mnt
+
 
 # all the above detach, so stay alive to keep the container running.
 while [ 1 ] ; do
    sleep 1d
 done
+
